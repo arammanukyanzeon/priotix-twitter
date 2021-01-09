2941ca2ea8f08bda4b552a28b37b8e962996dcc3
Aram <arammanukyanwork@gmail.com>
2021-01-09 16:20:18 +0400

    docker, knex edit

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:12.13.1
 WORKDIR /usr/app
 COPY package*.json ./
 RUN npm install
+ARG DOCKER_ENV
+ENV NODE_ENV=${DOCKER_ENV}
 ADD . /usr/app
-EXPOSE 8080
 CMD [ "npm", "start", "migrate_start" ]
--- db/knexfile.js
+++ db/knexfile.js
@@ -15,6 +15,22 @@ const conf = {
     migrations: {
       tableName: 'knex_migrations'
     }
+  },
+  production: {
+    client: 'postgres',
+    connection: {
+      database: 'priotix',
+      user: 'postgres',
+      password: 'qwerty1',
+      host: 'priotix'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations_lda'
+    }
   }
 };
 export default conf;
